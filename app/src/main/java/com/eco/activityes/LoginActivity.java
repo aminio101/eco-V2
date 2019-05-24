b8da5ec143b50cfde655b8ce491a7a1d6ab9f469
AmirhoseinEbrahimi <amirhosen.ebrahimi@gmail.com>
2019-05-24 11:26:38 -0700

    login animation

--- app/src/main/java/com/eco/activityes/LoginActivity.java
+++ app/src/main/java/com/eco/activityes/LoginActivity.java
@@ -1,9 +1,20 @@
 package com.eco.activityes;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
+import android.support.constraint.Guideline;
 import android.support.v7.app.AppCompatActivity;
+import android.transition.ChangeBounds;
+import android.transition.Transition;
+import android.transition.TransitionManager;
 import android.view.View;
+import android.view.animation.OvershootInterpolator;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
@@ -17,27 +28,83 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class LoginActivity extends AppCompatActivity implements ILoginView{
-    @OnClick(R.id.login_button_send_code) public void sendCode(){
+public class LoginActivity extends AppCompatActivity implements ILoginView {
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @OnClick(R.id.login_button_send_code)
+    public void sendCode() {
         presenter.sendCode(editTextPhoneNumber.getText().toString());
+        onBackClick();
+        View view1 = getCurrentFocus();
+        if (view1 != null) {
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view1.getWindowToken(), 0);
+        }
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    @OnClick(R.id.editText_login_number)
+    public void onClick() {
+        onApplyClick();
+    }
+
     @BindView(R.id.login_progress_circular_send_code)
-    ProgressBar progressBar ;
+    ProgressBar progressBar;
+    @BindView(R.id.guideline7)
+    Guideline guidelineTop;
     @BindView(R.id.editText_login_number)
     EditText editTextPhoneNumber;
     @BindView(R.id.login_button_send_code)
     Button buttonSendCode;
+    @BindView(R.id.root)
+    ConstraintLayout root;
     LoginPresenter presenter;
+    ConstraintSet constraintSetOld;
+    ConstraintSet constraintSetNew;
+    private boolean openKeyboard;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         init();
     }
-    private void init(){
-        ButterKnife.bind(this);
-        presenter = new LoginPresenter(this,this,progressBar,buttonSendCode);
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void onApplyClick() {
+        Transition transition = new ChangeBounds();
+        transition.setInterpolator(new OvershootInterpolator());
+        transition.setDuration(2000);
+        TransitionManager.beginDelayedTransition(root, transition);
+
+        if (!openKeyboard) {
+            constraintSetNew.applyTo(root);
+            openKeyboard = true;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void onBackClick() {
+        Transition transition = new ChangeBounds();
+        transition.setInterpolator(new OvershootInterpolator());
+        transition.setDuration(2000);
+        TransitionManager.beginDelayedTransition(root, transition);
+
+        if (openKeyboard) {
+            constraintSetOld.applyTo(root);
+            openKeyboard = false;
+        }
+    }
+
+
+    private void init() {
+        ButterKnife.bind(this);
+        presenter = new LoginPresenter(this, this, progressBar, buttonSendCode);
+        constraintSetNew = new ConstraintSet();
+        constraintSetOld = new ConstraintSet();
+        constraintSetOld.clone(root);
+        constraintSetNew.clone(this, R.layout.activity_login_2);
+        openKeyboard = false;
     }
 
     @Override
@@ -58,7 +125,7 @@ public class LoginActivity extends AppCompatActivity implements ILoginView{
 
     @Override
     public void goToVeryFyCodeActivity() {
-        Intent intent =new Intent(LoginActivity.this, VerifyCodeActivity.class);
+        Intent intent = new Intent(LoginActivity.this, VerifyCodeActivity.class);
         intent.putExtra("phone", editTextPhoneNumber.getText().toString());
         startActivity(intent);
     }
---
+++ app/src/main/java/com/eco/fragments/MainFragment.java
@@ -0,0 +1,20 @@
+package com.eco.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.eco.R;
+
+public class MainFragment extends Fragment {
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.main_fragment, container, false);
+
+        return view;
+    }
+}
